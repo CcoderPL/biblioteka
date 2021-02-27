72ab46d3f00729545120798f57d3ecb6f5aec978
CcoderPL <pawel.molek@gmail.com>
2021-02-27 13:54:11 +0100

    borrow history added

--- history.php
+++ history.php
@@ -27,54 +27,47 @@
       <body>
             <?php
                   //opcja wylogowania
-            	echo "<p style='margin-left:60%'>Witaj ".$_SESSION['user'].'! <a href="logout.php">Wyloguj się!</a> &nbsp;&nbsp; <a href="index.php">Strona Główna</a> &nbsp;&nbsp;<a href="history.php">Historia Wypożyczeń</a></p>';
+            	echo "<p style='margin-left:60%'>Witaj ".$_SESSION['user'].'! <a href="logout.php">Wyloguj się!</a> &nbsp;&nbsp; <a href="index.php">Strona Główna</a> &nbsp;&nbsp;<a href="wypozyczalnia.php">Moje konto</a></p>';
 
-                  $zapytanieWypozyczenia = "SELECT wypozyczenia.borrowid, wypozyczenia.borrowdate, wypozyczenia.userid, wypozyczenia.bookid, ksiazki.bookid, ksiazki.title, uzytkownicy.user, uzytkownicy.userid
-                  FROM wypozyczenia, ksiazki, uzytkownicy WHERE uzytkownicy.userid = wypozyczenia.userid AND
-                  ksiazki.bookid = wypozyczenia.bookid AND wypozyczenia.userid = {$_SESSION['userid']}";
+                  $zapytanieHistorii = "SELECT zwroty.returndate, zwroty.borrowdate, zwroty.userid, zwroty.bookid, ksiazki.bookid, ksiazki.title, uzytkownicy.user, uzytkownicy.userid
+                  FROM zwroty, ksiazki, uzytkownicy WHERE uzytkownicy.userid = zwroty.userid AND
+                  ksiazki.bookid = zwroty.bookid AND zwroty.userid = {$_SESSION['userid']}";
 
                   //wysłanie zapytania do bazy, konieczne jest do tego korzystanie z zmiennej weryfikującej połączenie z bazą
-                  $rezultatWypozyczenia = mysqli_query($polaczenie, $zapytanieWypozyczenia);
+                  $rezultatHistorii = mysqli_query($polaczenie, $zapytanieHistorii);
 
                   //zmienna potrzebna do wykonania pętli, sprawdza ile rzędów zwróciło zapytanie
-                  $ile = mysqli_num_rows($rezultatWypozyczenia);
-            ?>
-            <form action='zwrot.php' method='post'>
-                  <?php
+                  $ile = mysqli_num_rows($rezultatHistorii);
                         // uporzadkować tabelę w indexie !!!!!!!!!!!!!!!!!
                         if ($ile>=1)
                         {
-                              echo "<table width='600px' align='center' border='1'>
+                              echo "<table width='700px' align='center' border='1'>
                                    <tr>
                                           <th width='250px'>Tytuł</th>
                                           <th width='250px'>Data wypożyczenia</th>
-                                          <th width='100px'>Zwróć</th>
+                                          <th width='200px'>Data Zwrotu</th>
                                    </tr>";
                               //pętle wyświetlająca wszystkie zwrócone z zapytania wpisy
                               for ($i = 1; $i <= $ile; $i++)
                               {
                                     //pobranie rzędu jako tablicę asocjacyjną
-                                    $row = mysqli_fetch_assoc($rezultatWypozyczenia);
+                                    $row = mysqli_fetch_assoc($rezultatHistorii);
                                     //przypisanie zmiennych
                                     $title = $row['title'];
-                                    $date = $row['borrowdate'];
-                                    $bookid = $row['bookid'];
+                                    $borrowDate = $row['borrowdate'];
+                                    $returnDate = $row['returndate'];
                                     echo
                                           "<tr>
                                                 <td> ".$title." </td>
-                                                <td> ".$date." </td>
-                                                <td>
-                                                      <input type='checkbox' name='idzwroconejksiazki[]' value='$bookid'/>
-                                                </td>
+                                                <td> ".$borrowDate." </td>
+                                                <td> ".$returnDate." </td>
                                           </tr>";
                               }
                               echo "</table>";
-                              echo "<br/>";
-                              echo "<input style='margin-left:75%' type='submit' name='zapis' value='Zwracam'/>";
                         }
                         else
                         {
-                              echo "Aktualnie nie masz wypożyczonej żadnej książki";
+                              echo "Nie wypożyczałeś żadnej książki";
                         }
                         /*
                         //wyswietlenie 1 rekordu -- dramat
@@ -87,6 +80,5 @@
                             echo $row['userid']."<br>";
                       }*/
                   ?>
-            </form>
       </body>
 </html>
